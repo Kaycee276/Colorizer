5b4cc3f57736f026afa0a8c4768463b7162c87b0
Kaycee276 <kizuaba@gmail.com>
2024-09-28 17:31:14 +0100

    It generates random colours

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = function () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+        rowGap: "5rem",
+        height: "95vh",
+        alignItems: "center",
+        background: "rgb(210,153,127)",
+        borderRadius: "1rem",
+        overflow: "scroll",
+      }}
+    >
+      <Sect />
+      <Sect />
+      <Sect />
     </div>
   );
-}
+};
+
+const Sect = function () {
+  const [backgroundColor, setbackgroundColor] = useState("255, 255, 225");
+
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+
+  const [Text, setText] = useState(`rgb(${backgroundColor})`);
+  const handleClick = () => {
+    setbackgroundColor(`${red}, ${green},  ${blue}`);
+    setText(`rgb(${red} ${green}, ${blue})`);
+  };
+
+  return (
+    <section
+      style={{
+        height: "200px",
+      }}
+    >
+      <ColorPallete
+        Text={Text}
+        handleClick={handleClick}
+        backgroundColor={backgroundColor}
+      />
+    </section>
+  );
+};
+
+const ColorPallete = function ({ Text, handleClick, backgroundColor }) {
+  return (
+    <div
+      style={{
+        height: "100%",
+        background: `rgb(${backgroundColor})`,
+        display: "grid",
+        placeItems: "center",
+        userSelect: "none",
+        cursor: "pointer",
+        fontSize: "10px",
+        border: "3px solid black",
+        borderRadius: "10px",
+        padding: "1rem",
+        textAlign: "center",
+        transition: "all ease-in-out .4s",
+        textTransform: "uppercase",
+      }}
+      onClick={handleClick}
+    >
+      <h1>{Text}</h1>
+    </div>
+  );
+};
 
 export default App;
